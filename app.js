ce10b5b519010a88a836ef6e19e6b910d77e5a46
Mateo Rumac <85076814+mateorumac@users.noreply.github.com>
2024-08-05 13:30:20 +0200

    Update app.js
    changed web link

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(cors({
   credentials: true,
-  origin: 'https://wa-frontend-mateorumac.vercel.app'
+  origin: 'https://frontend-wa.vercel.app/'
 }));
 
 app.use(bodyParser.json())
